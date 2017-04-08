d526968c6bb1d26b296fec56fb9a9bc24dc5a2e3
Flo <florian@jphilippi.de>
2017-04-08 10:04:29 -0700

    Update advanced_python_csv
    Tabs = 4

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -5,6 +5,7 @@ def get_emails(file):
 	#emails = str(df['email']).split('\n')
 	faculty = open('/Users/flowinger/ds/metis/metisgh/prework/dsp/python/faculty.csv')
 	file2 = []
+	
 	for line in faculty:
 		line = line.strip()
 		file2.append(line)
@@ -15,6 +16,7 @@ print(get_emails(file0))
 
 def write_to_csv(res):
 	csv_file = '/Users/flowinger/ds/metis/metisgh/prework/dsp/python/emails.csv'
+	
 	with open(csv_file, 'w') as csvfile:
 		writer = csv.writer(csvfile, lineterminator='\n')
 		for email in res:
